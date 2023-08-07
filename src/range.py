7cf1e0e8bf9be7c54558108a1636f1d806ef6170
Aidan <aidan.lowson@davies-group.com>
2023-08-07 17:41:20 +0100

    for loops, range and tuples

---
+++ src/range.py
@@ -0,0 +1,2 @@
+for num in range(5, 10, 2):# Start at index 5, go up to 10 in steps of 2
+  print(num)
---
+++ src/tuples.py
@@ -0,0 +1,5 @@
+# Tuples are like lists but they're not mutable, we cannot change them once they've been initiated
+
+numbers = (1, 2, 3, 3)
+numbers.count(3)
+numbers.index(3)
